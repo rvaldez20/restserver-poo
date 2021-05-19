155dad1db90fbdac8118020b3fffa67921fbfaf6
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-05-19 15:11:54 -0500

    Se hace validaciona para el login: 1]que el email exista, 2]que este activo el usuario y 3]que el password sea correcto

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,54 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
 
 // función para las accines del login
-const login = (req=request, res=response) => {
+const login = async(req=request, res=response) => {
 
+   const { correo, password } = req.body;
+
+   try {
+
+      // Acciones a considerar para la validacion
+      // (1) verificar si el email
+      const usuario = await Usuario.findOne({ correo });
+      if(!usuario) {
+         return res.status(400).json({
+            msg: 'Usuario / Password no son correctos - correo'
+         });
+      }
+
+      // (2) verificar si el usuario esta activo
+      if(usuario.estado === false) {
+         return res.status(400).json({
+            msg: 'Usuario / Password no son correctos - estado: false'
+         });
+      }
+
+      // (3) si existe email, verificar si el password es correcto
+      // bcryptjs.compareSync compara el passwor que s emanda oontra el guadaddo en la db
+      const passwordValido = bcryptjs.compareSync(password, usuario.password);
+      if(!passwordValido){
+         return res.status(400).json({
+            msg: 'Usuario / Password no son correctos - password'
+         });
+      }
+
+      // (4) generar el JWT
+      
+      
+      res.json({
+         msg: 'Login OK',
+      });
+
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         msg: 'Algo salio mal, reportalo al administrador'
+      })
+   }
    
-
-   res.json({
-      msg: 'Login OK'
-   });
 }
 
 
