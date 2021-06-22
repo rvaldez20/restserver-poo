64e40d8be15d0aa05581d08681cc6afa7acb1f02
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-06-22 17:04:44 -0500

    Se configura el servicio  cloudinary para guardar los las imagenes de los productos y usuarios

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,5 +1,9 @@
 const { response, request } = require("express");
 const { subirArchivo } = require("../helpers");
+
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL );
+
 const  path = require('path');
 const fs = require('fs');
 
@@ -26,7 +30,7 @@ const cargarArchivo = async (req, res = response) => {
 }
 
 
-
+// es para hacerlo en prueba con localhost
 const actualizarImagen = async (req = request, res = response) => {
 
    // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
@@ -73,6 +77,71 @@ const actualizarImagen = async (req = request, res = response) => {
 
    const nombre = await subirArchivo( req.files, undefined, coleccion );
    modelo.imagen = nombre;
+   await modelo.save()
+
+   res.json({
+      modelo
+   })
+}
+
+
+// Es para guardar las imagene sen elservicio de cloudinary
+const actualizarImagenCloudinary = async (req = request, res = response) => {
+   
+   const { id, coleccion } = req.params;
+   let modelo;
+
+   switch (coleccion) {
+      case 'usuarios':
+         modelo = await Usuario.findById( id );
+         if ( !modelo ){
+            return res.status(400).json({
+               msg: `No existe un usuario con ese ID ${id}`
+            })
+         }
+         
+         break;
+
+      case 'productos':
+         modelo = await Producto.findById( id );
+         if ( !modelo ){
+            return res.status(400).json({
+               msg: `No existe un producto con ese ID ${id}`
+            })
+         }
+         
+         break;
+   
+      default:
+         return res.status(500).json({msg: 'Se me olvido validar esto'})
+   }
+
+   // validaciones para eliminar archivos inecesarios (limpia imagenes previas)
+   if (modelo.imagen) {
+      // hay que borrar la imagen del servidor  
+      // Primero extrar el name qu ele asino cloudinary del secure_url
+      const nombreArr = modelo.imagen.split('/');      
+      const nombreConExtension = nombreArr[ nombreArr.length - 1 ];
+
+      // SOLUCION con substring
+      // const public_id = nombreConExtension.substring(0, nombreArr[7].length-4);
+
+      // SOLUCION con destructuracion y split
+      const [ public_id ] = nombreConExtension.split('.');
+      
+      // console.log(nombreArr[7]);
+      // console.log(public_id);
+
+      // Eliminamos la imagen (se pued equitar o dejar el await pq no necesitamos espera  que la borre)
+      cloudinary.uploader.destroy( public_id );      
+   }
+
+   // console.log(req.files.archivo);
+   const { tempFilePath } = req.files.archivo;
+   const resp = await cloudinary.uploader.upload( tempFilePath );
+   const { secure_url } = resp;
+   
+   modelo.imagen = secure_url;
    await modelo.save()
 
    res.json({
@@ -129,5 +198,6 @@ const mostrarImagen = async (req, res = response) => {
 module.exports = {
    cargarArchivo,
    actualizarImagen,
-   mostrarImagen
+   mostrarImagen,
+   actualizarImagenCloudinary
 }
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads.controller');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads.controller');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
 
@@ -18,7 +18,8 @@ router.put('/:coleccion/:id', [
    check('id', 'No es un ID de usuario valido (mongoId)').isMongoId(),
    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] )),
    validarCampos
-], actualizarImagen);
+], actualizarImagenCloudinary);
+// ], actualizarImagen);
 
 // ruta para obtener imagen 
 router.get('/:coleccion/:id',[
